41ae0395ff8dd87174d0551df975bdebeb35352a
migin <miginmrs@protonmail.com>
2020-08-13 15:19:21 +0100

    add test

---
+++ test/index.spec.ts
@@ -0,0 +1,22 @@
+import { expect } from 'chai';
+import { of, Observable } from 'rxjs';
+import { TestScheduler } from 'rxjs/testing';
+import { share, switchMap, tap } from 'rxjs/operators';
+import { alternMap } from '../source/index';
+
+const testScheduler = () => new TestScheduler((actual, expected) => {
+  expect(actual).deep.equal(expected);
+});
+
+describe('AlternMap', function () {
+  it('should unsubscribe to new observable after subscribing to new observable', function () {
+    testScheduler().run(({ expectObservable, cold }) => {
+      const obs = cold('abc').pipe(share())
+      expectObservable(cold('a-b').pipe(switchMap(() => obs))).toBe('ababc');
+    })
+    testScheduler().run(({ expectObservable, cold }) => {
+      const obs = cold('abc').pipe(share())
+      expectObservable(cold('a-b').pipe(alternMap(() => obs))).toBe('abc');
+    })
+  })
+})
